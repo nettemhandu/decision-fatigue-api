842e82367bfdd24cf0677627c1de9a1107068830
nettemhandu <antonetteliebe@icloud.com>
2026-03-03 19:52:22 +0200

    initial commit

---
+++ src/main/java/com/antonett/decisionfatigue/DecisionfatigueApplication.java
@@ -0,0 +1,13 @@
+package com.antonett.decisionfatigue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionfatigueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionfatigueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonett/decisionfatigue/DecisionfatigueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonett.decisionfatigue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionfatigueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
